c67b05938c512a9ba35b62431d37e74544658b78
Habl <habltaker@gmail.com>
2025-06-20 19:39:48 +0300

    5task

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,10 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const bgdColor = document.querySelector(".color");
+const btn = document.querySelector(".change-color");
+btn.addEventListener("click", (event) => {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  bgdColor.textContent = color;
+})
